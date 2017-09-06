9d32fbd35f0f7a457b45474452a9e37b2f479876
Dominic Maas <Dominic.maas@live.com>
2017-09-06 19:08:35 +1200

    Added Client

---
+++ Tango/Networking/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.Networking
+{
+    public class Client : IDisposable
+    {
+        #region Setup
+        private static Client _serverInstance;
+        public static Client Instance => _serverInstance ?? (_serverInstance = new Client());
+        #endregion
+
+
+        public void Dispose()
+        {
+          
+        }
+
+        ~Client()
+        {
+            Dispose();
+        }
+    }
+}
