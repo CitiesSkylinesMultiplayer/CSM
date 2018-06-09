434104ec8ddc2b72d0a08a400622af3d80a7e88b
Dominic Maas <dominic.maas@live.com>
2018-06-09 14:40:50 +1200

    Move to a different networking library, use a console project for testing.

---
+++ CitiesSkylinesMultiplayer.ClientTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using LiteNetLib;
+
+namespace CitiesSkylinesMultiplayer.ClientTest
+{
+    class Program
+    {
+        #region Private Variables
+        private EventBasedNetListener _listener;
+        private NetManager _netClient;
+        #endregion
+
+        static void Main(string[] args)
+        {
+            new Program().Start(args);
+        }
+
+        private void Start(string[] args)
+        {
+            // Setup the listener and bind events
+            _listener = new EventBasedNetListener();
+            _listener.NetworkReceiveEvent += _listener_NetworkReceiveEvent;
+            _listener.NetworkErrorEvent += ListenerOnNetworkErrorEvent;
+
+            // Create the client
+            _netClient = new NetManager(_listener, "TangoAlpha");
+            var result = _netClient.Start();
+
+            // Connect the client
+            var connection = _netClient.Connect(IpBox.Text, int.Parse(PortBox.Text));
+
+        }
+
+        private void ListenerOnNetworkErrorEvent(NetEndPoint endpoint, int socketerrorcode)
+        {
+            // On Error, show a message box
+            Console.WriteLine($"[{endpoint.Host}:{endpoint.Port}] Socket Error Code: {socketerrorcode}");
+        }
+
+        private void _listener_NetworkReceiveEvent(NetPeer peer, LiteNetLib.Utils.NetDataReader reader)
+        {
+            Console.WriteLine($"[{peer.EndPoint.Host}:{peer.EndPoint.Port}] {reader.GetString()}");
+        }
+    }
+}
