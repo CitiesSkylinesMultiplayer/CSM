2c2a49d9620f25272a06838509a2b4bc402521f0
Dominic Maas <dominic.maas@live.com>
2018-06-11 18:09:34 +1200

    Fix simulation

--- src/csm/Extensions/ThreadingExtension.cs
+++ src/csm/Extensions/ThreadingExtension.cs
@@ -8,25 +8,36 @@ namespace CSM.Extensions
 {
     public class ThreadingExtension : ThreadingExtensionBase
     {
+        private int _lastSelectedSimulationSpeed;
+        private bool _lastSimulationPausedState;
+
+
         public override void OnAfterSimulationTick()
         {
-            switch (MultiplayerManager.Instance.CurrentRole)
+            if (_lastSimulationPausedState != SimulationManager.instance.ForcedSimulationPaused ||
+                _lastSelectedSimulationSpeed != SimulationManager.instance.SelectedSimulationSpeed)
             {
-                case MultiplayerRole.Client:
-                    MultiplayerManager.Instance.CurrentClient.NetClient.GetFirstPeer().Send(ArrayHelpers.PrependByte(CommandBase.SimulationCommandID, new SimulationCommand
-                    {
-                        SumulationSpeed = SimulationManager.instance.SelectedSimulationSpeed,
-                        SimulationPaused = SimulationManager.instance.SimulationPaused
-                    }.Serialize()), SendOptions.ReliableOrdered);
-                    break;
-                case MultiplayerRole.Server:
-                    MultiplayerManager.Instance.CurrentServer.NetServer.SendToAll(ArrayHelpers.PrependByte(CommandBase.SimulationCommandID, new SimulationCommand
-                    {
-                        SumulationSpeed = SimulationManager.instance.SelectedSimulationSpeed,
-                        SimulationPaused = SimulationManager.instance.SimulationPaused
-                    }.Serialize()), SendOptions.ReliableOrdered);
-                    break;
+                switch (MultiplayerManager.Instance.CurrentRole)
+                {
+                    case MultiplayerRole.Client:
+                        MultiplayerManager.Instance.CurrentClient.NetClient.GetFirstPeer().Send(ArrayHelpers.PrependByte(CommandBase.SimulationCommandID, new SimulationCommand
+                        {
+                            SumulationSpeed = SimulationManager.instance.SelectedSimulationSpeed,
+                            SimulationPaused = SimulationManager.instance.SimulationPaused
+                        }.Serialize()), SendOptions.ReliableOrdered);
+                        break;
+                    case MultiplayerRole.Server:
+                        MultiplayerManager.Instance.CurrentServer.NetServer.SendToAll(ArrayHelpers.PrependByte(CommandBase.SimulationCommandID, new SimulationCommand
+                        {
+                            SumulationSpeed = SimulationManager.instance.SelectedSimulationSpeed,
+                            SimulationPaused = SimulationManager.instance.SimulationPaused
+                        }.Serialize()), SendOptions.ReliableOrdered);
+                        break;
+                }
             }
+
+            _lastSelectedSimulationSpeed = SimulationManager.instance.SelectedSimulationSpeed;
+            _lastSimulationPausedState = SimulationManager.instance.SimulationPaused;
 
             base.OnAfterSimulationTick();
         }
