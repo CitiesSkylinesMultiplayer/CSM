8f9e714e43b47934163a07031bee5db38348ad69
Sander Jochems <sanderjochems@hotmail.nl>
2018-10-27 12:16:10 +0200

    Update src/Networking/IPAddress.cs

--- src/Networking/IPAddress.cs
+++ src/Networking/IPAddress.cs
@@ -7,5 +7,21 @@ namespace CSM.Networking
     public class IPAddress
     {
 
+        public static string GetLocalIPAddress()
+        {
+            try
+            {
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                {
+                    socket.Connect("8.8.8.8", 65530);
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    return endPoint.Address.ToString();
+                }
+            }
+            catch (Exception)
+            {
+                return "Not found";
+            }
+        }
     }
 }
