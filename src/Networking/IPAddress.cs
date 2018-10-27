db16dfe1fedadc9a3080e6c763c0a11da54a1735
Sander Jochems <sanderjochems@hotmail.nl>
2018-10-27 13:10:51 +0200

    Update src/Networking/IPAddress.cs

--- src/Networking/IPAddress.cs
+++ src/Networking/IPAddress.cs
@@ -11,15 +11,20 @@ namespace CSM.Networking
         {
             try
             {
+                //Create a new socket
                 using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
                 {
+                    //Connect to 8.8.8.8 (Google IP)
                     socket.Connect("8.8.8.8", 65530);
+                    //Get the IPEndPoint
                     IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    //Get the IP Address (Internal) from the IPEndPoint
                     return endPoint.Address.ToString();
                 }
             }
             catch (Exception)
             {
+                //On error return "Not found"
                 return "Not found";
             }
         }
@@ -28,10 +33,12 @@ namespace CSM.Networking
         {
             try
             {
-                return new WebClient().DownloadString("https://api.ipify.org");
+                //Get the External IP (IPv4) Address from internet
+                return new WebClient().DownloadString("http://api.ipify.org"); //HTTPS doesnt work
             }
             catch (Exception)
             {
+                //On error return "Not found"
                 return "Not found";
             }
         }
