696489473ded5be803e56e93b578b4fbfb9eede1
Treholt3103 <41299009+Treholt3103@users.noreply.github.com>
2018-11-03 00:24:50 +0100

    This Sync the Node and SegmentNodes (Roads, Waterpipes, powerlines ect)
    This adds a new extension that syncs Nodes and SegmentNodes, it uses the ThreadingExtensionBase and use a new Thread to check for changes in the Nodes and sending these Changes.
    
    For the Extension to function, the reciever needs to first recieve all the Nodes before recieving the NodesSegment. I have tried to ensure this by adding Thread.Sleep and using locks, but this is in no way a "safe" implementation that ensures that all is recieved and handled in the right order. Before release we need a new and more stable way of handling this.

---
+++ src/Commands/Data/NodeCommand.cs
@@ -0,0 +1,19 @@
+﻿using ProtoBuf;
+using UnityEngine;
+
+namespace CSM.Commands
+{
+	[ProtoContract]
+	public class NodeCommand : CommandBase
+	{
+		[ProtoMember(1)]
+		public Vector3 Position { get; set; }
+
+		[ProtoMember(2)]
+		public ushort InfoIndex { get; set; }
+
+		[ProtoMember(3)]
+		public int NodeID { get; set; }
+
+	}
+}
---
+++ src/Commands/Handler/NodeHandler.cs
@@ -0,0 +1,59 @@
+﻿using ColossalFramework;
+using CSM.Networking;
+using System.Collections.Generic;
+using CSM.Helpers;
+
+namespace CSM.Commands.Handler
+{
+	class NodeHandler : CommandHandler<NodeCommand>
+	{
+
+		public override byte ID => 109;
+
+		public override void HandleOnClient(NodeCommand command)
+		{
+			Extensions.NodeandSegmentExtension._NetSegmentLocked = true;
+			lock (Extensions.NodeandSegmentExtension._netSegment)
+			{
+				
+
+				NetInfo netinfo = PrefabCollection<NetInfo>.GetPrefab(command.InfoIndex);
+				lock (Extensions.NodeandSegmentExtension.NodeIDDictionary)
+				{
+					lock (Extensions.NodeandSegmentExtension.VectorDictionary)
+					{
+						Singleton<NetManager>.instance.CreateNode(out ushort node, ref Singleton<SimulationManager>.instance.m_randomizer, netinfo, command.Position, Singleton<SimulationManager>.instance.m_currentBuildIndex);
+						Extensions.NodeandSegmentExtension.NodeIDDictionary.Add((ushort)command.NodeID, node); //Adds the NodeID recived and the NodeID generated on NodeCreation.
+						Extensions.NodeandSegmentExtension.VectorDictionary.Add(command.Position, node);
+
+					}
+				}
+				
+			}
+			Extensions.NodeandSegmentExtension._NetSegmentLocked = false;
+		}
+
+		public override void HandleOnServer(NodeCommand command, Player player)
+		{
+			Extensions.NodeandSegmentExtension._NetSegmentLocked = true;
+			lock (Extensions.NodeandSegmentExtension._netSegment)
+			{
+				
+
+				NetInfo netinfo = PrefabCollection<NetInfo>.GetPrefab(command.InfoIndex);
+				lock (Extensions.NodeandSegmentExtension.NodeIDDictionary)
+				{
+					lock (Extensions.NodeandSegmentExtension.VectorDictionary)
+					{
+						Singleton<NetManager>.instance.CreateNode(out ushort node, ref Singleton<SimulationManager>.instance.m_randomizer, netinfo, command.Position, Singleton<SimulationManager>.instance.m_currentBuildIndex);
+						Extensions.NodeandSegmentExtension.NodeIDDictionary.Add((ushort)command.NodeID, node);
+						Extensions.NodeandSegmentExtension.VectorDictionary.Add(command.Position, node);
+					}
+				}
+
+				
+			}
+			Extensions.NodeandSegmentExtension._NetSegmentLocked = false;
+		}
+	}
+}
