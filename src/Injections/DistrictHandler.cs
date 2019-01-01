4d426a303cffc1b70f5fcb914d01b75b55ebc70c
Treholt3103 <41299009+Treholt3103@users.noreply.github.com>
2019-01-01 22:33:10 +0100

    Sync Creation, modification and release of districts
    This syncs the Creation, modification and release of districts. It does not sync policies.
    
    Also it contains a small bug the name of the district will not show on creation , to make the name update the reciver of the district has to click on it using the district brush, the same is true for updating the names position and removing the name.

---
+++ src/Injections/DistrictHandler.cs
@@ -0,0 +1,74 @@
+﻿using ColossalFramework;
+using CSM.Commands;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using static DistrictTool;
+
+namespace CSM.Injections
+{
+    class DistrictHandler
+    {
+        public static List<ushort> IgnoreDistricts { get; } = new List<ushort>();
+        public static List<Vector3> IgnoreAreaModified { get; } = new List<Vector3>();
+    }
+
+
+    [HarmonyPatch(typeof(DistrictManager))]
+    [HarmonyPatch("CreateDistrict")]
+    public class CreateDistrict
+    {
+        public static void Postfix(bool __result, ref byte district)
+        {
+            if (__result && !DistrictHandler.IgnoreDistricts.Contains(district))
+            {
+                Command.SendToAll(new DistrictCreateCommand
+                {
+                    DistrictID = district
+                });
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DistrictTool))]
+    [HarmonyPatch("ApplyBrush")]
+    [HarmonyPatch(new Type[] { typeof(Layer), typeof(byte), typeof(float), typeof (Vector3), typeof(Vector3)})]
+    public class ApplyBrush
+    {
+        public static void Postfix (Layer layer, byte district, float brushRadius, Vector3 startPosition, Vector3 endPosition)
+        {
+            if (!DistrictHandler.IgnoreAreaModified.Contains(startPosition))
+            {
+                Command.SendToAll(new DistrictAreaModifyCommand
+                {
+                    Layer = layer,
+                    District = district,
+                    BrushRadius = brushRadius,
+                    StartPosition = startPosition,
+                    EndPosition = endPosition
+                });
+                
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DistrictManager))]
+    [HarmonyPatch("ReleaseDistrict")]
+    public class ReleaseDistrict
+    {
+        public static void Postfix(byte district)
+        {
+            if (!DistrictHandler.IgnoreDistricts.Contains(district))
+            {
+                Command.SendToAll(new DistrictReleaseCommand
+                {
+                    DistrictID = district,
+                });
+            }
+        }
+    }
+
+
+}
