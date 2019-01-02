8077866ff66154bd29c2bd109b9fb98c5f167404
Treholt3103 <41299009+Treholt3103@users.noreply.github.com>
2019-01-02 22:02:22 +0100

    Set District and City policy
    This syncs the the setting of city and district policy, however it does not yet contain the unsetting of them so you have to manually remove the policies. (the unsetting code should not take long to write, i just don't have the time tonight)

---
+++ src/Commands/Handler/CityPolicyHandler.cs
@@ -0,0 +1,21 @@
+﻿using CSM.Injections;
+using CSM.Networking;
+
+namespace CSM.Commands.Handler
+{
+    public class CityPolicyHandler : CommandHandler<CityPolicyCommand>
+    {
+        public override byte ID => CommandIds.CityPolicyCommand;
+
+        public override void HandleOnClient(CityPolicyCommand command) => Handle(command);
+
+        public override void HandleOnServer(CityPolicyCommand command, Player player) => Handle(command);
+
+        private void Handle(CityPolicyCommand command)
+        {
+            DistrictHandler.ignoreCityPolicy = true;
+            DistrictManager.instance.SetCityPolicy(command.Policy);
+            DistrictHandler.ignoreCityPolicy = false;
+        }
+    }
+}
