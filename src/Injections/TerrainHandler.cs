9c065912cfd33cb5cc558e48214cf23ca061a38d
Treholt3103 <41299009+Treholt3103@users.noreply.github.com>
2019-01-13 16:17:50 +0100

    Working terrainsync

--- src/Injections/TerrainHandler.cs
+++ src/Injections/TerrainHandler.cs
@@ -17,12 +17,13 @@ namespace CSM.Injections
 [HarmonyPatch("ApplyBrush")]
 public class ApplyTerrainBrush
     {
-        public static void Postfix(ref float ___m_strength)
+        public static void Postfix()//ref float ___m_strength, ref float ___m_brushSize, ref Vector3 ___m_startPosition, ref Vector3 ___m_endPosition, ref bool ___m_mouseLeftDown, ref bool ___m_mouseRightDown, ref Vector3 m_mousePosition, ref Mode ___m_mode, ref int ___m_strokeXmin, ref int ___m_strokeXmax, ref int ___m_strokeZmin, ref int ___m_strokeZmax)
         {
             if (!TerrainHandler.ignoreTerrainModify)
             {
-                UnityEngine.Debug.Log($"Applyed Brush, strength {___m_strength}");
-                /*Command.SendToAll(new TerrainModificationCommand
+                UnityEngine.Debug.Log($"Applyed Brush, strength");
+                
+                Command.SendToAll(new TerrainModificationCommand
                 {
                     BrushData = Singleton<ToolController>.instance.BrushData,//(float[])typeof(ToolController).GetField("BrushData", AccessTools.all).GetValue(Singleton<ToolController>.instance), //ï'm preatty sure this one is wrong, but its not this one that is causing the problem
                     BrushSize = Singleton<TerrainTool>.instance.m_brushSize,
@@ -38,7 +39,8 @@ public class ApplyTerrainBrush
                     StrokeZmin = (int)typeof(TerrainTool).GetField("m_strokeZmin", AccessTools.all).GetValue(ToolsModifierControl.GetTool<TerrainTool>()),
                     StrokeZmax = (int)typeof(TerrainTool).GetField("m_strokeZmax", AccessTools.all).GetValue(ToolsModifierControl.GetTool<TerrainTool>()),
                 });
-                */
+                
+                
             }
         }
     }
