d6d35d3f85e6a3350c8c8ad84ba010fd11b731bc
kaenganxt <kaenganxt@mc-anura.de>
2019-12-23 13:39:55 +0100

    Add Linux and MacOS support to install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,35 @@
 # Installer script to be bundled with release files
 param (
-    [string]$ModDirectory = "$env:LOCALAPPDATA\Colossal Order\Cities_Skylines\Addons\Mods\CSM"
+    [string]$ModDirectory = "Default"
 )
+
+# Assume windows, since PowerShell 6 is the first with official cross-platform support
+If ($PSVersionTable.PSVersion.Major -lt 6)
+{
+    $IsWindows = $true
+}
+
+$Sep = "\"
+If (($IsMacOS) -or ($IsLinux))
+{
+    $Sep = "/"
+}
+
+If ($ModDirectory -eq "Default")
+{
+    If ($IsLinux)
+    {
+        $ModDirectory = "~/.local/share/Colossal Order/Cities_Skylines/Addons/Mods/CSM"
+    }
+    ElseIf ($IsMacOS)
+    {
+        $ModDirectory = "~/Library/Application Support/Colossal Order/Cities_Skylines/Addons/Mods/CSM"
+    }
+    ElseIf ($IsWindows)
+    {
+        $ModDirectory = "$env:LOCALAPPDATA\Colossal Order\Cities_Skylines\Addons\Mods\CSM"
+    }
+}
 
 Write-Host "Installing..."
 
@@ -12,12 +40,12 @@ Remove-Item $ModDirectory -Recurse -ErrorAction Ignore
 New-Item -ItemType directory -Path $ModDirectory | Out-Null
 
 # Copy the items
-Copy-Item -Path "LiteNetLib.dll"        -Destination "$($ModDirectory)\LiteNetLib.dll" -Force
-Copy-Item -Path "protobuf-net.dll"      -Destination "$($ModDirectory)\protobuf-net.dll" -Force
-Copy-Item -Path "CSM.dll"               -Destination "$($ModDirectory)\CSM.dll" -Force
-Copy-Item -Path "Open.Nat.dll"          -Destination "$($ModDirectory)\Open.Nat.dll" -Force
-Copy-Item -Path "System.Threading.dll"  -Destination "$($ModDirectory)\System.Threading.dll" -Force
-Copy-Item -Path "0Harmony.dll"          -Destination "$($ModDirectory)\0Harmony.dll" -Force
-Copy-Item -Path "NLog.dll"         	-Destination "$($ModDirectory)\NLog.dll" -Force
+Copy-Item -Path "LiteNetLib.dll"        -Destination "$($ModDirectory)$($Sep)LiteNetLib.dll" -Force
+Copy-Item -Path "protobuf-net.dll"      -Destination "$($ModDirectory)$($Sep)protobuf-net.dll" -Force
+Copy-Item -Path "CSM.dll"               -Destination "$($ModDirectory)$($Sep)CSM.dll" -Force
+Copy-Item -Path "Open.Nat.dll"          -Destination "$($ModDirectory)$($Sep)Open.Nat.dll" -Force
+Copy-Item -Path "System.Threading.dll"  -Destination "$($ModDirectory)$($Sep)System.Threading.dll" -Force
+Copy-Item -Path "0Harmony.dll"          -Destination "$($ModDirectory)$($Sep)0Harmony.dll" -Force
+Copy-Item -Path "NLog.dll"         	-Destination "$($ModDirectory)$($Sep)NLog.dll" -Force
 
 Write-Host "Mod is now installed, open Cities Skylines and enable the mod."
