69996afcdd91a7c6f10065db4cf3314ceee453b4
Dominic Maas <dominic.maas@live.com>
2019-12-23 13:08:01 +1300

    Add settings file, UI and ability to toggle debug logging (false by default)

---
+++ src/Panels/SettingsPanel.cs
@@ -0,0 +1,17 @@
+﻿using ColossalFramework.UI;
+using ICities;
+
+namespace CSM.Panels
+{
+    public static class SettingsPanel
+    {
+        public static void Build(UIHelperBase helper, Settings settings)
+        {
+            var advancedGroup = helper.AddGroup("Advanced");
+
+            UICheckBox cb = (UICheckBox)advancedGroup.AddCheckbox("Enable debug logging (requires game restart)", settings.DebugLogging.value,
+                c => { settings.DebugLogging.value = c; });
+            cb.tooltip = "Note: This may cause excessive logging and slow down the game!";
+        }
+    }
+}
