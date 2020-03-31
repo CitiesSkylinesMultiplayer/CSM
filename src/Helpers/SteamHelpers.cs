129ca482bb8b60b30a9d8b3d06c1f0d8b513457d
Dominic Maas <dominic.maas@live.com>
2020-03-31 20:22:32 +1300

    Attempt to make this work

--- src/Helpers/SteamHelpers.cs
+++ src/Helpers/SteamHelpers.cs
@@ -1,27 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace CSM.Helpers
 {
     public static class SteamHelpers
     {
+        private static IntPtr _friendsPtr;
+
         public static bool Init()
         {
-            return SteamAPI_Init();
+            var res = SteamAPI_Init();
+            if (!res) return false;
+
+
+            // Setup steam friends
+            _friendsPtr = SteamFriends();
+            if (_friendsPtr == IntPtr.Zero) 
+                throw new Exception("SteamFriends returned 0");
+
+            return true;
+
         }
 
         public static void SetRichPresence(string key, string value)
         {
-
+            if (_friendsPtr == IntPtr.Zero) return;
         }
-
 
         // EXTERN
 
         [DllImport("steam_api64", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         private static extern bool SteamAPI_Init();
+
+        [DllImport("steam_api64", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern IntPtr SteamFriends();
+
+        [DllImport("steam_api64", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private static extern bool SteamAPI_ISteamFriends_SetRichPresence(IntPtr instancePtr, [MarshalAs(UnmanagedType.LPStr)] string pchKey, [MarshalAs(UnmanagedType.LPStr)] string pchValue);
     }
 }
