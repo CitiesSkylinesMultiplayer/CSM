ce3834f53ec72fa59e98a1cd0039ac168261ca8e
dfrankes <dfrankes@users.noreply.github.com>
2020-07-13 01:10:09 +0200

    Fix player pointer issues (#161)
    * Fix exceptions from #155
    * Fix #164 crash on joining
    
    Co-authored-by: Nico Borgsmüller <kaenganxt@mc-anura.de>

--- src/Injections/ICameraHandler.cs
+++ src/Injections/ICameraHandler.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 namespace CSM.Injections
 {
     [HarmonyPatch(typeof(CameraController))]
-    [HarmonyPatch("UpdateTransform")]
+    [HarmonyPatch("LateUpdate")]
     public class ICameraUpdateCurrentPosition
     {
         private static Vector3 playerCameraPosition_last;
@@ -16,27 +16,47 @@ namespace CSM.Injections
 
         public static void Postfix(CameraController __instance, Camera ___m_camera)
         {
-            // Get camera rotation, angle and position
-            Transform transform = ___m_camera.transform;
-            Quaternion _rotation = transform.rotation;
-            Vector3 _position = transform.position;
-
-            // Check if the camera moved
-            if (Vector3.Distance(_position, playerCameraPosition_last) > 1 || playerCameraRotation_last != _rotation)
+            // Make sure the camera object is defined, this should always happen but for some strange reason it didnt (look bellow)
+            // This is not happening if i test it localy. (debug using AllocConsole in CSM.cs)
+            // https://github.com/DominicMaas/Tango/issues/155
+            if (___m_camera && MultiplayerManager.Instance.CurrentRole != MultiplayerRole.None)
             {
-                // Store camera rotation and position
-                playerCameraPosition_last = _position;
-                playerCameraRotation_last = _rotation;
+                // Get camera rotation, angle and position
+                Transform transform = ___m_camera.transform;
+                Quaternion _rotation = transform.rotation;
+                Vector3 _position = transform.position;
 
-                // Send info to all clients
-                Command.SendToAll(new PlayerLocationCommand
+                // Update the player camera position every time the camera moves OR rotates
+                // This should not be that demanding on the server (might be demanding if you are playing with a lot of players
+                // We could also make the server update the player locations every x seconds by storing all the info on the server and send it to all clients every 1 second?
+                if (Vector3.Distance(_position, playerCameraPosition_last) > 1 || playerCameraRotation_last != _rotation)
                 {
-                    PlayerName = MultiplayerManager.Instance.CurrentClient.Config.Username,
-                    PlayerCameraPosition = _position,
-                    PlayerCameraRotation = _rotation,
-                    PlayerCameraHeight = __instance.m_currentHeight,
-                    PlayerColor = JoinGamePanel.playerColor
-                });
+                    // Store camera rotation and position
+                    playerCameraPosition_last = _position;
+                    playerCameraRotation_last = _rotation;
+
+
+                    // Set the correct playerName if our currentRole is SERVER, else use the CurrentClient Username
+                    string playerName;
+                    if (MultiplayerManager.Instance.CurrentRole == MultiplayerRole.Server)
+                    {
+                        playerName = MultiplayerManager.Instance.CurrentServer.Config.Username;
+                    }
+                    else
+                    {
+                        playerName = MultiplayerManager.Instance.CurrentClient.Config.Username;
+                    }
+
+                    // Send info to all clients
+                    Command.SendToAll(new PlayerLocationCommand
+                    {
+                        PlayerName = playerName,
+                        PlayerCameraPosition = _position,
+                        PlayerCameraRotation = _rotation,
+                        PlayerCameraHeight = __instance.m_currentHeight,
+                        PlayerColor = JoinGamePanel.playerColor
+                    });
+                }
             }
         }
     }
