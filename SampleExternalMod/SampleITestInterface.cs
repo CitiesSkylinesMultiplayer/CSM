5531cc595475a8dd85fc0385ee3b733cd1e43afb
MightyWizard <mightywizard07@gmail.com>
2021-06-25 13:49:26 +0100

    Sample External Mod updated

--- SampleExternalMod/SampleITestInterface.cs
+++ SampleExternalMod/SampleITestInterface.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CSM.API;
+using NLog;
 
 namespace SampleExternalMod
 {
     class SampleITestInterface : ITest
     {
+        public Guid HandlerID => new Guid();
+
         public string Handle()
         {
-            throw new NotImplementedException();
+            string test = "This is a sample Mod";
+            Logger _logger = LogManager.GetLogger("CSM");
+            _logger.Info(test);
+            return test;
         }
     }
 }
