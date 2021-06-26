d5651c223e096e3ccd6e3c9ae99ab469cf8f3110
João Lourenco <joaoguerralourenco@hotmail.com>
2021-06-26 11:17:57 +0100

    Creating Test scenario for sending and receiving.

---
+++ SampleExternalMod/Injections/MainMenuHandler.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using NLog;
+using SampleExternalMod;
+using SampleExternalMod.Commands;
+
+namespace CSM.Injections
+{
+
+    [HarmonyPatch(typeof(DistrictManager))]
+    [HarmonyPatch("CreateDistrict")]
+    public class CreateDistrict
+    {
+        private static readonly Logger _logger = LogManager.GetLogger("CSM");
+        
+        public static void Postfix(bool __result, ref byte district)
+        {
+            _logger.Info("Doing something");
+            CSMConnection.Instance.SentToAll(new TestCommand());
+        }
+    }
+    
+    [HarmonyPatch(typeof(MainMenu))]
+    [HarmonyPatch("Awake")]
+    public class MainMenuAwake
+    {
+        /// <summary>
+        ///     This handler is used to place the 'JOIN GAME' button to the start
+        ///     of the main menu (handles reloading the intro etc.
+        /// </summary>
+        public static void Prefix()
+        {
+            MainMenuHandler.CreateOrUpdateJoinGameButton();
+        }
+    }
+
+    public static class MainMenuHandler
+    {
+        private static readonly Logger _logger = LogManager.GetLogger("CSM");
+
+        public static void CreateOrUpdateJoinGameButton()
+        {
+            _logger.Info("Doing something");
+            CSMConnection.Instance.SentToAll(new TestCommand());
+        }
+    }
+}
--- SampleExternalMod/SampleUserMod.cs
+++ SampleExternalMod/SampleUserMod.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Reflection;
+using HarmonyLib;
 using ICities;
+using NLog;
 
 namespace SampleExternalMod
 {
     public class SampleUserMod : IUserMod
-    {
+    {        
+        private static readonly Logger _logger = LogManager.GetLogger("CSM");
+        private Harmony _harmony;
+        
         public string Name
         {
             get { return "Sample External Mod"; }
@@ -13,6 +19,28 @@ namespace SampleExternalMod
         public string Description
         {
             get { return "Adds Nothing"; }
+        }
+
+        public void OnEnabled()
+        {
+            try
+            {
+                _harmony = new Harmony("com.mytest");
+                _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("[TEST] Patching failed", ex);
+            }
+
+            _logger.Info("[TEST] Construction Complete!");
+        }
+
+        public void OnDisabled()
+        {
+            _logger.Info("[TEST] Unpatching Harmony...");
+            _harmony.UnpatchAll();
+            _logger.Info("[TEST] Destruction complete!");
         }
     }
 }
