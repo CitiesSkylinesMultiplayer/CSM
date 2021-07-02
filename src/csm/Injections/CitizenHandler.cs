4ff63520d278a61626f8e8cca8cdf182a5a2a64b
jlourenco <joaoguerralourenco.pt@gmail.com>
2021-07-02 12:09:08 +0100

    Added now the releases.

--- src/csm/Injections/CitizenHandler.cs
+++ src/csm/Injections/CitizenHandler.cs
@@ -1,17 +1,15 @@
-using CSM.Networking;
-using HarmonyLib;
 using System;
 using ColossalFramework.Math;
+using CSM.Networking;
+using HarmonyLib;
 
 namespace CSM.Injections
 {
-    
     [HarmonyPatch(typeof(CitizenManager))]
     [HarmonyPatch("CreateUnits")]
     // public bool CreateUnits(out uint firstUnit, ref Randomizer randomizer, ushort building, ushort vehicle, int homeCount, int workCount, int visitCount, int passengerCount, int studentCount)
     public class CreateUnits
     {
-        
         public static bool Prefix(ref bool __result)
         {
             if (MultiplayerManager.Instance.CurrentRole != MultiplayerRole.Server)
@@ -19,17 +17,16 @@ namespace CSM.Injections
                 __result = true;
                 return false;
             }
+
             return true;
         }
-        
     }
-    
+
     [HarmonyPatch(typeof(CitizenManager))]
     [HarmonyPatch("CreateCitizen")]
     // public bool CreateCitizen(out uint citizen, int age, int family, ref Randomizer r)
     public class CreateCitizenWithoutGender
     {
-        
         public static bool Prefix(ref bool __result)
         {
             if (MultiplayerManager.Instance.CurrentRole != MultiplayerRole.Server)
@@ -37,18 +34,17 @@ namespace CSM.Injections
                 __result = true;
                 return false;
             }
+
             return true;
         }
-        
     }
-    
+
     [HarmonyPatch(typeof(CitizenManager))]
     [HarmonyPatch("CreateCitizen")]
-    [HarmonyPatch(new Type[] { typeof(uint), typeof(int), typeof(int), typeof(Randomizer), typeof(Citizen.Gender) })]
+    [HarmonyPatch(new Type[] {typeof(uint), typeof(int), typeof(int), typeof(Randomizer), typeof(Citizen.Gender)})]
     // public bool CreateCitizen(out uint citizen, int age, int family, ref Randomizer r, Citizen.Gender gender)
     public class CreateCitizenWithGender
     {
-        
         public static bool Prefix(ref bool __result)
         {
             if (MultiplayerManager.Instance.CurrentRole != MultiplayerRole.Server)
@@ -56,18 +52,17 @@ namespace CSM.Injections
                 __result = true;
                 return false;
             }
+
             return true;
         }
-        
     }
-    
+
     [HarmonyPatch(typeof(CitizenManager))]
     [HarmonyPatch("CreateCitizenInstance")]
-    [HarmonyPatch(new Type[] { typeof(ushort), typeof(Randomizer), typeof(CitizenInfo), typeof(uint) })]
+    [HarmonyPatch(new Type[] {typeof(ushort), typeof(Randomizer), typeof(CitizenInfo), typeof(uint)})]
     // public bool CreateCitizenInstance( out ushort instance, ref Randomizer randomizer, CitizenInfo info, uint citizen)
     public class CreateCitizenInstance
     {
-        
         public static bool Prefix(ref bool __result)
         {
             if (MultiplayerManager.Instance.CurrentRole != MultiplayerRole.Server)
@@ -75,9 +70,42 @@ namespace CSM.Injections
                 __result = true;
                 return false;
             }
+
             return true;
         }
-        
     }
-    
+
+    [HarmonyPatch(typeof(CitizenManager))]
+    [HarmonyPatch("ReleaseUnitCitizen")]
+    // private void ReleaseUnitCitizen(uint unit, ref CitizenUnit data, uint citizen)
+    public class ReleaseUnitCitizen
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            if (MultiplayerManager.Instance.CurrentRole != MultiplayerRole.Server)
+            {
+                __result = true;
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(CitizenManager))]
+    [HarmonyPatch("ReleaseCitizenInstanceImplementation")]
+    // private void ReleaseCitizenInstanceImplementation(ushort instance, ref CitizenInstance data)
+    public class ReleaseCitizenInstanceImplementation
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            if (MultiplayerManager.Instance.CurrentRole != MultiplayerRole.Server)
+            {
+                __result = true;
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
