1a73f00cadbcd91ffbdaa30d5a4e3b309adf783b
Ronald Mourik <ronald@codingplaza.nl>
2021-07-15 22:51:49 +0200

    Change log level without restart

--- src/Util/Log.cs
+++ src/Util/Log.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+﻿using ColossalFramework.UI;
+using NLog;
 using NLog.Config;
 using NLog.Targets;
 using System;
@@ -31,19 +32,38 @@ namespace CSM.Util
             // Target for console logging
             ConsoleTarget logConsole = new ConsoleTarget("logconsole") { Layout = layout };
 
+            // Target for console logging
+            //MethodCallTarget logDebugConsole = new MethodCallTarget("logdebugconsole", (logEvent, o) =>
+            //{
+            //    if (logEvent.Level == LogLevel.Error)
+            //        DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Error, logEvent.FormattedMessage);
+            //    else if (logEvent.Level == LogLevel.Warn)
+            //        DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Warning, logEvent.FormattedMessage);
+            //    else
+            //        DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, logEvent.FormattedMessage);
+            //});
+
             // If debug logging is enabled
             if (debug)
             {
+                //config.AddRule(LogLevel.Debug, LogLevel.Fatal, logDebugConsole);
                 config.AddRule(LogLevel.Debug, LogLevel.Fatal, logConsole);
                 config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
             }
             else
             {
+                //config.AddRule(LogLevel.Info, LogLevel.Fatal, logDebugConsole);
                 config.AddRule(LogLevel.Info, LogLevel.Fatal, logConsole);
                 config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
             }
 
             LogManager.Configuration = config;
+        }
+
+        public static void ChangeLogLevel(LogLevel level)
+        {
+            LogManager.Configuration.LoggingRules.ForEach(x => x.SetLoggingLevels(level, LogLevel.Fatal));
+            LogManager.ReconfigExistingLoggers();
         }
 
         public static void Error(string message)
