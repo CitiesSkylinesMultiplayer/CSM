0f3c35ee0f2132f720754a3c200817bf59597423
Nico Borgsmüller <me@kaenganxt.dev>
2022-01-28 22:55:32 +0100

    Add tree segment update injection

--- src/basegame/Injections/TreeHandler.cs
+++ src/basegame/Injections/TreeHandler.cs
@@ -63,4 +63,32 @@ namespace CSM.BaseGame.Injections
             });
         }
     }
+    
+    // Sync updating of tree types in roads
+    [HarmonyPatch(typeof(TreeTool))]
+    [HarmonyPatch("UpgradeSegmentImpl")]
+    public class UpdateTreeType
+    {
+        public static void Prefix(ToolBase.ToolErrors ___m_placementErrors, ushort ___m_upgradeSegment, TreeInfo ___m_prefab, Vector3 ___m_mousePosition)
+        {
+            if (IgnoreHelper.Instance.IsIgnored())
+            {
+                return;
+            }
+
+            if (___m_placementErrors != ToolBase.ToolErrors.None)
+            {
+                return;
+            }
+
+            ushort prefab = (ushort)Mathf.Clamp(___m_prefab.m_prefabDataIndex, 0, 65535);
+
+            Command.SendToAll(new TreeUpgradeSegmentCommand
+            {
+                UpgradeSegment = ___m_upgradeSegment,
+                Prefab = prefab,
+                MousePosition = ___m_mousePosition
+            });
+        }
+    }
 }
