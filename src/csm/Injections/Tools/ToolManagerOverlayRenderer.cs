cc9ca60b6b640f060e273b7415b20ba9bb045222
Connor Moody <cwmlolzlz@gmail.com>
2022-04-21 19:36:44 +1200

    Fixed NetTool helper lines not render for other client NetTools

--- src/csm/Injections/Tools/ToolManagerOverlayRenderer.cs
+++ src/csm/Injections/Tools/ToolManagerOverlayRenderer.cs
@@ -2,6 +2,7 @@ using ColossalFramework;
 using System.Collections.Generic;
 using HarmonyLib;
 using CSM.BaseGame.Helpers;
+using CSM.API.Helpers;
 
 namespace CSM.Injections.Tools
 {
@@ -25,9 +26,22 @@ namespace CSM.Injections.Tools
         
         public static void Postfix(RenderManager.CameraInfo cameraInfo)
         {
+            // the NetTool uses static fields to share guidelines between render and simulation loops
+            // unsetting the static field here will prevent any NetTools from clients also rendering the
+            // guidelines again
+
+            var currentHelperLines = NetTool.m_helperLines;
+            NetTool.m_helperLines = new FastList<NetTool.HelperLine>();
+
+            // var currentDirectionArrows = ReflectionHelper.GetAttr((NetTool)null, "m_directionArrowsBuffer");
+            // ReflectionHelper.SetAttr((NetTool)null, "m_directionArrowsBuffer", new FastList<NetTool.HelperDirection2>());
+
             foreach (var simulatedTool in Singleton<ToolSimulator>.instance.GetTools()) {
                 simulatedTool.RenderOverlay(cameraInfo);
             }
+
+            NetTool.m_helperLines = currentHelperLines;
+            // ReflectionHelper.SetAttr((NetTool)null, "m_directionArrowsBuffer", currentDirectionArrows);
         }
 
     }
