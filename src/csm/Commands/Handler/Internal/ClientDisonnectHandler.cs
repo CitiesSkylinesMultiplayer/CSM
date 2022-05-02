732e9a16ab605245bfec4e979970ec281e0b283f
Connor Moody <cwmlolzlz@gmail.com>
2022-05-02 21:11:34 +1200

    Added removal of player cursors when they disconnect

--- src/csm/Commands/Handler/Internal/ClientDisonnectHandler.cs
+++ src/csm/Commands/Handler/Internal/ClientDisonnectHandler.cs
@@ -4,6 +4,7 @@ using CSM.API.Networking;
 using CSM.BaseGame.Helpers;
 using CSM.Commands.Data.Internal;
 using CSM.Networking;
+using CSM.Injections.Tools;
 using ColossalFramework;
 
 namespace CSM.Commands.Handler.Internal
@@ -24,6 +25,7 @@ namespace CSM.Commands.Handler.Internal
 
             TransactionHandler.ClearTransactions(command.ClientId);
             Singleton<ToolSimulator>.instance.RemoveSender(command.ClientId);
+            Singleton<ToolSimulatorCursorManager>.instance.RemoveCursorView(command.ClientId);
         }
 
         public override void OnClientDisconnect(Player player)
