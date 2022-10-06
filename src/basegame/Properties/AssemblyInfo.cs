57b76f8d93fb7b1225a52d7f45b83cdf84c932a6
Dominic Maas <dominic.maas@live.com>
2022-10-07 08:37:06 +1300

    Change company in assembly files, order mods so SequenceOrder will work.

--- src/basegame/Properties/AssemblyInfo.cs
+++ src/basegame/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Cities: Skylines Multiplayer Mod Basegame Logic")]
 [assembly: AssemblyDescription("BaseGame logic for Cities: Skylines Multiplayer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Grid Entertainment")]
+[assembly: AssemblyCompany("CSM Developers")]
 [assembly: AssemblyProduct("Cities: Skylines Multiplayer Mod")]
 [assembly: AssemblyCopyright("Copyright © CSM Developers 2022")]
 [assembly: AssemblyTrademark("")]
